ecb675f9f31806133f184834ed370468da2d3f21
Themis <thelareular@gmail.com>
2022-04-17 19:38:34 +0300

    exp1_final

--- monitor.py
+++ monitor.py
@@ -2,7 +2,7 @@ from predictor import predictor
 
 if __name__ == "__main__":
     LAG = 58 # days to look back
-    PATH = 'C:\\Users\\Themis\\Desktop\\Cuda_bitcoin_pred\\models\\model_exp1' # model path
+    PATH = './model_exp1' # model path
     pred = predictor(LAG = 58) #object predictor
     signal, prediction, end_date, prev_close, prev_date = pred.predict(PATH=PATH) #predict
 
