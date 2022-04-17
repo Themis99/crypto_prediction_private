595427bd7836a2c34f174c76578f43b512eb8886
goxr3plus <crAlexcomp@gmail.com>
2022-04-17 20:37:33 +0300

    Fixed warnings + added model path for each prediction

--- monitor.py
+++ monitor.py
@@ -1,10 +1,17 @@
 from predictor import predictor
 
+model = 'model_exp1'
 if __name__ == "__main__":
-    LAG = 58 # days to look back
-    PATH = './model_exp1' # model path
-    pred = predictor(LAG = 58) #object predictor
-    signal, prediction, end_date, prev_close, prev_date = pred.predict(PATH=PATH) #predict
+    LAG = 58  # days to look back
+    PATH = './' + model  # model path
+    pred = predictor(LAG=58)  # object predictor
+    signal, prediction, end_date, prev_close, prev_date = pred.predict(PATH=PATH)  # predict
 
-    print('BTC AT: '+str(prediction)+' FOR '+end_date+' SIGNAL: '+signal+' PREVIOUS CLOSE AT: '+str(prev_close)+' FOR '+prev_date)
+    print('Model : ' + model
+          + ' BTC Price: ' + str(prediction)
+          + ' FOR ' + end_date
+          + ' UP/DOWN: ' + signal
+          + ' PREVIOUS CLOSE AT: ' + str(prev_close)
+          + ' FOR '+prev_date
+          )
 
