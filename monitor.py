db8421e5526f7bcddd793acf07347a8440e1ced9
Themis <thelareular@gmail.com>
2022-04-19 09:57:16 +0300

    add winloss monitor

--- monitor.py
+++ monitor.py
@@ -1,48 +1,36 @@
-from data_exporter import data_exporter
 from predictor import predictor
+import data_collector
 
+def winloss(prev_pred,prev_s):
 
-# Predict using the given model
-def predict(model):
-    # days to look back
-    lag = 58
+    #take data
+    data = data_collector.retrieve_data()
+    #take prev close
+    close_r = round(data.tail(1)['Close'].item(),2)
+    #take comp day
+    close_p = round(data[:-1].tail(1)['Close'].item(),2)
 
-    # model path
-    path = './' + model
+    real_s = 'UP' if close_p < close_r else 'DOWN'
 
-    # object predictor
-    pred = predictor(LAG=58)
-    signal, prediction, end_date, prev_close, prev_date = pred.predict(PATH=path)  # predict
+    trade = 'WIN' if real_s == prev_s else 'LOSS'
+    out = abs(close_r - prev_pred) if real_s == prev_s else None
 
-    signalString = '⬆️' if signal == 'UP' else '⬇️'
-    message = 'Using model : [ ' + model + ' ]'\
-             ' \n\nBTC price : [ ' + str(prediction) + ' $ ] '\
-             ' For Date : [ ' + end_date + ' ]'\
-             ' \n\nDirection : ' + signalString + \
-             ' \n\nPREVIOUS CLOSE AT: [ ' + str(prev_close) + ' $ ] ' \
-             ' For Date : [ ' + prev_date + ' ]'
-
-    print(message)
-
-    # ------------------------------ THE BELOW CODE IS FOR THE TELEGRAM BOT ------------------------------
-    # Get Chat id for our bot with apikey = 5145257581:AAFFag1OAu9fR5KE0YTHsY2303z8CF-o6To
-    # https://api.telegram.org/bot5145257581:AAFFag1OAu9fR5KE0YTHsY2303z8CF-o6To/getUpdates
-    # chat id = -1001720397362
-
-    chat_id = '-1001720397362'
-    base_url = 'https://api.telegram.org/bot5145257581:AAFFag1OAu9fR5KE0YTHsY2303z8CF-o6To/sendMessage?chat_id=' + chat_id + '&text=' + message
-
-    # requests.get(base_url)
-
-    # -------------------- THE BELOW CODE IS FOR EXPORTING THE DATA TO JSON FILE -------------------------
-
-    # Export data to json file for each unique day we run this program
-    data_exporter.export_data(False, model, prediction, prev_close, signal, prev_date, end_date)
-
+    return trade,out
 
 if __name__ == "__main__":
+    LAG = 58 # days to look back
+    PATH = 'C:\\Users\\Themis\\Desktop\\Cuda_bitcoin_pred\\models\\model_exp1_alt' # model path
+    pred = predictor(LAG = 58) #object predictor
+    signal, prediction, end_date, prev_close, prev_date = pred.predict(PATH=PATH) #predict
 
-    predict('model_exp1')
-    predict('model_exp1_alt')
+    print('BTC AT: '+str(prediction)+' FOR '+end_date+' SIGNAL: '+signal+' PREVIOUS CLOSE AT: '+str(prev_close)+' FOR '+prev_date)
 
+
+    prev_p = 39960 #χθεσινη προβλεψη (θα τα περνεις απο json)
+    prev_s = 'UP' #χθεσινο σημα (θα το περνεις απο json
+
+    initial_trade = None # Αν κανουμε την πρωτη μας προβλεψη (πχ με ενα νεο μποτ) επειδη δεν εχουμε χθεσινη (προηγουμενη) προβλεψη πρεπει να αλλαξουμε την συνθηκη με το χερι
+
+    if initial_trade is not None:
+        winloss_yes,out = winloss(prev_p,prev_s) #γυρναει win αν αν ηταν σωστο το signal (δηλαδη αν επεσε η ανεβηκε και οντως το προβλεψε σωστα) και out ποσο μακρια επεσε. Αν επεσε εξω τοτε το out ειναι None
 
