97959a43dddc644951fa90bef94a5f50a2eaf329
goxr3plus <crAlexcomp@gmail.com>
2022-04-21 03:33:45 +0300

    Fixes

--- src/data_exporter_dir/alex_monitor.py
+++ src/data_exporter_dir/alex_monitor.py
@@ -1,10 +1,11 @@
 from src import data_collector
+from src.data_exporter_dir import data_exporter_library
 from src.predictors.predictor_1 import predictor_1
 from src.predictors.predictor_2 import predictor_2
 from src.predictors.predictor_3 import predictor_3
 
 
-def winloss(prev_pred, prev_s):
+def winloss(previous_prediction, previous_signal):
     # take data
     data = data_collector.retrieve_data()
     # take prev close
@@ -14,16 +15,15 @@ def winloss(prev_pred, prev_s):
 
     real_s = 'UP' if close_p < close_r else 'DOWN'
 
-    trade = 'WIN' if real_s == prev_s else 'LOSS'
-    out = abs(close_r - prev_pred) if real_s == prev_s else None
+    trade = 'WIN' if real_s == previous_signal else 'LOSS'
+    out = abs(close_r - previous_prediction) if real_s == previous_signal else None
 
     return trade, out
 
 
 # Predict using the given model
 def predict(model):
-    # days to look back
-    lag = 58
+    #  LAG = days to look back
 
     # model path
     path = '../models/' + model
@@ -31,11 +31,11 @@ def predict(model):
     # object predictor
     pred = ''
 
-    if (model == 'model_exp1'):
+    if (model == 'model_1'):
         pred = predictor_1(LAG=58)
-    elif (model == 'model_exp2'):
+    elif (model == 'model_2'):
         pred = predictor_2(LAG=58)
-    elif (model == 'model_exp3'):
+    elif (model == 'model_3'):
         pred = predictor_3(LAG=48)
 
     signal, prediction, end_date, prev_close, prev_date = pred.predict(PATH=path)  # predict
@@ -48,8 +48,9 @@ def predict(model):
              ' \n\nBTC closing price : [ ' + str(prev_close) + ' $ ] ' \
              ' For Date : [ ' + prev_date + ' ] 4 am Greek Time'
 
-    print(message)
+    # print(message)
 
+# print(winloss())
     # ------------------------------ THE BELOW CODE IS FOR THE TELEGRAM BOT ------------------------------
     # Get Chat id for our bot with apikey = 5145257581:AAFFag1OAu9fR5KE0YTHsY2303z8CF-o6To
     # https://api.telegram.org/bot5145257581:AAFFag1OAu9fR5KE0YTHsY2303z8CF-o6To/getUpdates
@@ -63,10 +64,10 @@ def predict(model):
     # -------------------- THE BELOW CODE IS FOR EXPORTING THE DATA TO JSON FILE -------------------------
 
     # Export data to json file for each unique day we run this program
-    # data_exporter_library.export_data(False, model, prediction, prev_close, signal, prev_date, end_date)
+    data_exporter_library.export_data(False, model, prediction, prev_close, signal, prev_date, end_date)
 
 
 if __name__ == "__main__":
-    predict('model_exp1')
-    predict('model_exp2')
-    predict('model_exp3')
+    predict('model_1')
+    predict('model_2')
+    predict('model_3')
